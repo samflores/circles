3e9244f9a66b007544109bfe0c538e0ef1598843
Samuel Flores <me@samflor.es>
2024-04-15 14:41:41 -0300

    Configure base template
    - Add static files used by template (css, js, images, fonts)
    - Add bootstrap
    - Add popperjs
    - Update default layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+// import "@hotwired/turbo-rails"
+
+window.process = { env: {} };
+
+import "@popperjs/core"
+import "bootstrap"
+
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelectorAll('[data-bs-toggle="tooltip"]')
+    .forEach(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+});
