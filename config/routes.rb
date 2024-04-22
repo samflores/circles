f932cff9f5c2286805796a95ecb8cda24f06f2ab
Samuel Flores <me@samflor.es>
2024-04-22 15:11:58 -0300

    Tweaks in preparation for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'profiles/show'
   # https://guides.rubyonrails.org/routing.html
 
   get 'news', to: 'newsfeeds#index'
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  resources :profiles
+  resources :profiles, only: %i[show]
 
   # root "posts#index"
 end
